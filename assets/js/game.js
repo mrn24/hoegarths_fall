6200bc66076d801662106d0be2fa37c8d5949a3e
mrn24 <mrn24@nau.edu>
2016-05-25 18:11:03 -0700

    Made Dungeon. Using premade character. Boundaries

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,27 +1,134 @@
-// Tutorial code from Dr. Palmer's YouTube video : https://www.youtube.com/watch?v=grlg4I-Bt24
+var GAME_WIDTH = 400;
+var GAME_HEIGHT = 400;
+var GAME_SCALE = 1;
 var gameport = document.getElementById("gameport");
 
-var renderer = PIXI.autoDetectRenderer(400, 400, {backgroundColor: 0x3344ee});
+//Whole map is 1920
+
+var renderer = PIXI.autoDetectRenderer(GAME_WIDTH, GAME_HEIGHT, {backgroundColor: 0x000000});
 gameport.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
+stage.scale.x = GAME_SCALE;
+stage.scale.y = GAME_SCALE;
 
-var texture = PIXI.Texture.fromImage("./assets/img/cs413.png");
 
-var sprite = new PIXI.Sprite(texture);
+var game = new PIXI.Container();
 
-sprite.anchor.x = 0.5;
-sprite.anchor.y = 0.5;
+var currentRoomx;
+var currentRoomy;
+//Directions:
+//1=North, 2= East, 3= South, 4= West
+var pathCorrect= [2, 3, 4, 1, 3, 4, 1, 2, 1, 2, 4, 4, 3, 2, 3];
+var pathLeft;
+var dungeon;
 
-sprite.position.x = 200;
-sprite.position.y = 200;
+var player;
 
-stage.addChild(sprite);
+var MOVE_LEFT = 1;
+var MOVE_RIGHT = 2;
+var MOVE_UP = 3;
+var MOVE_DOWN = 4;
+var MOVE_NONE = 0;
+
+// The move function starts or continues movement
+function move() {
+  if (player.direction == MOVE_NONE) {
+    player.moving = false;
+    console.log("x:"+player.x);
+    console.log("y:"+player.y);
+    return;
+  }
+  player.moving = true;
+  console.log("move");
+
+  if (player.direction == MOVE_LEFT) {
+    createjs.Tween.get(player).to({x: player.x - 32}, 500).call(move);
+  }
+  if (player.direction == MOVE_RIGHT)
+    createjs.Tween.get(player).to({x: player.x + 32}, 500).call(move);
+
+  if (player.direction == MOVE_UP)
+    createjs.Tween.get(player).to({y: player.y - 32}, 500).call(move);
+
+  if (player.direction == MOVE_DOWN)
+    createjs.Tween.get(player).to({y: player.y + 32}, 500).call(move);
+}
+
+// Keydown events start movement
+window.addEventListener("keydown", function (e) {
+  e.preventDefault();
+  if (!player) return;
+  if (player.moving) return;
+  if (e.repeat == true) return;
+  if (player.position.x = (currentRoomx - 160) && player.position.y = currentRoomy){
+    console.log("moveRight");
+  }
+  if (player.position.x = (currentRoomx + 160) && player.position.y = currentRoomy){
+    console.log("moveLeft");
+  }
+  if (Math.abs(player.position.x - (currentRoomx - 16)) < 30 && player.position.y = currentRoomy + 160){
+    console.log("moveUp");
+  }
+  if (Math.abs(player.position.x - (currentRoomx - 16) && player.position.y = currentRoomy - 160){
+    console.log("moveDown");
+  }
+  player.direction = MOVE_NONE;
+
+  if (e.keyCode == 87)
+    player.direction = MOVE_UP;
+  else if (e.keyCode == 83)
+    player.direction = MOVE_DOWN;
+  else if (e.keyCode == 65)
+    player.direction = MOVE_LEFT;
+  else if (e.keyCode == 68)
+    player.direction = MOVE_RIGHT;
+
+  console.log(e.keyCode);
+  move();
+});
+
+// Keyup events end movement
+window.addEventListener("keyup", function onKeyUp(e) {
+  e.preventDefault();
+  if (!player) return;
+  player.direction = MOVE_NONE;
+});
+
+PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
+
+PIXI.loader
+  .add('room_json', './assets/img/dungeon.json')
+  .add('maptiles', './assets/img/maptiles.png')
+  .load(ready);
+
+function ready(){
+  var tu = new TileUtilities(PIXI);
+  dungeon = tu.makeTiledWorld("room_json", "./assets/img/maptiles.png");
+  game.addChild(dungeon);
+  stage.addChild(game);
+
+  player = new PIXI.Sprite.fromImage("./assets/img/cs413.png");
+  player.anchor.x = 0.0;
+  player.anchor.y = 1;
+  player.position.x = 960;
+  currentRoomx = 960;
+  player.position.y = 960;
+  currentRoomy = 960;
+  game.addChild(player);
+  update_camera();
+  animate();
+}
 
 function animate() {
   requestAnimationFrame(animate);
-  sprite.rotation += 0.1;
   renderer.render(stage);
 }
 
-animate();
+
+function update_camera() {
+  stage.x = -player.x*GAME_SCALE + GAME_WIDTH/2 - player.width/2*GAME_SCALE;
+  stage.y = -player.y*GAME_SCALE + GAME_HEIGHT/2 + player.height/2*GAME_SCALE;
+  stage.x = -Math.max(0, Math.min(dungeon.worldWidth*GAME_SCALE - GAME_WIDTH, -stage.x));
+  stage.y = -Math.max(0, Math.min(dungeon.worldHeight*GAME_SCALE - GAME_HEIGHT, -stage.y));
+}
