51fbbe4b4f93190cdf5509c2d56c018c10a084e3
mrn24 <mrn24@nau.edu>
2016-05-26 15:56:44 -0700

    fixed room transition, added clues.

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,10 +19,11 @@ var currentRoomx;
 var currentRoomy;
 //Directions:
 //1=North, 2= East, 3= South, 4= West
-var pathCorrect= [2, 3, 4, 1, 3, 4, 1, 2, 1, 2, 4, 4, 3, 2, 3];
-var pathLeft;
-var dungeon;
+var pathCorrect = [2, 3, 4, 1, 3, 4, 1, 2, 1, 2, 4, 4, 3, 2, 3];
+var pathLeft = pathCorrect.slice(0, 16);
 
+
+var dungeon;
 var player;
 
 var MOVE_LEFT = 1;
@@ -42,17 +43,60 @@ function move() {
   player.moving = true;
   console.log("move");
 
+  if (player.direction == MOVE_UP && Math.abs(player.position.x - (currentRoomx - 16)) < 30 && player.position.y == currentRoomy - 160){
+    console.log("moveUp");
+    player.direction == MOVE_NONE;
+    changeRoomUp();
+  }
+  if (player.direction == MOVE_LEFT && player.position.x == (currentRoomx - 160) && player.position.y == currentRoomy){
+    console.log("moveLeft");
+    player.direction == MOVE_NONE;
+    changeRoomLeft();
+  }
+  if (player.direction == MOVE_RIGHT && player.position.x == (currentRoomx + 160) && player.position.y == currentRoomy){
+    console.log("moveRight");
+    player.direction == MOVE_NONE;
+    changeRoomRight();
+  }
+  if (player.direction == MOVE_DOWN && Math.abs(player.position.x - (currentRoomx - 16)) < 30 && player.position.y == currentRoomy + 160){
+    console.log("moveDown");
+    player.direction = MOVE_NONE;
+    changeRoomDown();
+  }
+
+  if(player.direction == MOVE_UP && player.position.y == currentRoomy - 160){
+    console.log("Up wall");
+    player.direction = MOVE_NONE;
+    move();
+  }
+  if(player.direction == MOVE_LEFT && player.position.x == (currentRoomx - 160)){
+    console.log("Left wall");
+    player.direction = MOVE_NONE;
+    move();
+  }
+  if(player.direction == MOVE_RIGHT && player.position.x == (currentRoomx + 160)){
+    console.log("Right wall");
+    player.direction = MOVE_NONE;
+    move();
+  }
+  if(player.direction == MOVE_DOWN && player.position.y == currentRoomy + 160){
+    console.log("Down wall");
+    player.direction = MOVE_NONE;
+    move();
+  }
+
   if (player.direction == MOVE_LEFT) {
     createjs.Tween.get(player).to({x: player.x - 32}, 500).call(move);
   }
-  if (player.direction == MOVE_RIGHT)
+  if (player.direction == MOVE_RIGHT){
     createjs.Tween.get(player).to({x: player.x + 32}, 500).call(move);
-
-  if (player.direction == MOVE_UP)
+  }
+  if (player.direction == MOVE_UP){
     createjs.Tween.get(player).to({y: player.y - 32}, 500).call(move);
-
-  if (player.direction == MOVE_DOWN)
+  }
+  if (player.direction == MOVE_DOWN){
     createjs.Tween.get(player).to({y: player.y + 32}, 500).call(move);
+  }
 }
 
 // Keydown events start movement
@@ -61,18 +105,7 @@ window.addEventListener("keydown", function (e) {
   if (!player) return;
   if (player.moving) return;
   if (e.repeat == true) return;
-  if (player.position.x = (currentRoomx - 160) && player.position.y = currentRoomy){
-    console.log("moveRight");
-  }
-  if (player.position.x = (currentRoomx + 160) && player.position.y = currentRoomy){
-    console.log("moveLeft");
-  }
-  if (Math.abs(player.position.x - (currentRoomx - 16)) < 30 && player.position.y = currentRoomy + 160){
-    console.log("moveUp");
-  }
-  if (Math.abs(player.position.x - (currentRoomx - 16) && player.position.y = currentRoomy - 160){
-    console.log("moveDown");
-  }
+
   player.direction = MOVE_NONE;
 
   if (e.keyCode == 87)
@@ -94,6 +127,74 @@ window.addEventListener("keyup", function onKeyUp(e) {
   if (!player) return;
   player.direction = MOVE_NONE;
 });
+
+function changeRoomUp(){
+  createjs.Tween.get(dungeon).to({y: dungeon.y + 384}, 500);
+  createjs.Tween.get(player).to({y: player.y + 320}, 500).wait(700).call(function() {checkPos(1)});
+  //createjs.Tween.get(player).to({y: player.y - 32}, 500).wait(700).call(function() {checkPos(1)});
+  //currentRoomy += 384;
+  //player.position.y = player.position.y + 320;
+}
+function changeRoomDown(){
+  createjs.Tween.get(dungeon).to({y: dungeon.y - 384}, 500);
+  createjs.Tween.get(player).to({y: player.y - 320}, 500).wait(700).call(function() {checkPos(3)});
+  //createjs.Tween.get(player).to({y: player.y + 32}, 500).wait(700).call(function() {checkPos(3)});
+  //currentRoomy -= 384;
+  //player.position.y = player.position.y + 320;
+}
+function changeRoomRight(){
+  createjs.Tween.get(dungeon).to({x: dungeon.x - 384}, 500);
+  createjs.Tween.get(player).to({x: player.x - 320}, 500).wait(700).call(function() {checkPos(2)});
+  //createjs.Tween.get(player).to({x: player.x + 32}, 500).wait(700).call(function() {checkPos(2)});
+  //currentRoomx += 384;
+  //player.position.y = player.position.y + 320;
+}
+function changeRoomLeft(){
+  createjs.Tween.get(dungeon).to({x: dungeon.x + 384}, 500);
+  createjs.Tween.get(player).to({x: player.x + 320}, 500).wait(700).call(function() {checkPos(4)});
+  //createjs.Tween.get(player).to({x: player.x - 32}, 500).wait(700).call(function() {checkPos(4)});
+  //currentRoomx += 384;
+  //player.position.y = player.position.y + 320;
+}
+
+function checkPos(dir){
+  console.log("checkpos");
+  console.log("dir:"+dir);
+  console.log("gm:"+pathLeft[0]);
+  if(dir == pathLeft[0]){
+    console.log("Good Move!");
+    pathLeft.shift();
+    if(pathLeft.length == 0){
+      console.log("WIN!");
+    }
+  }
+  else{
+    console.log("Not a good move");
+    reset(dir);
+  }
+  player.direction = MOVE_NONE;
+  move();
+}
+
+function reset(dir){
+  console.log("reset");
+  pathLeft = pathCorrect.slice(0, 16);
+  dungeon.x = 0;
+  dungeon.y = 0;
+  if(dir == 1){
+    player.y = player.y;
+    player.x = player.x;
+  }else if(dir == 2){
+    player.y = player.y;
+    player.x = player.x;
+  }else if(dir == 3){
+    player.y = player.y;
+    player.x = player.x;
+  }else if(dir == 4){
+    player.y = player.y;
+    player.x = player.x;
+  }
+}
 
 PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
 
